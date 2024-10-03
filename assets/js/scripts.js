38c9fe6ee9b904e6c8fcbda853109bd410c2fe93
HannanBhuiyan <hannan.diuh@gmail.com>
2024-10-03 09:16:21 -0700

    upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,20 @@
+/*
+Author       :  
+Template Name:  
+Version      : 1.0
+*/
+(function($) {
+	'use strict';
+
+     
+	var swiper = new Swiper(".mySwiper", {
+		navigation: {
+		  nextEl: ".swiper-button-next",
+		  prevEl: ".swiper-button-prev",
+		},
+	  });
+   
+
+})(jQuery);
+
+
